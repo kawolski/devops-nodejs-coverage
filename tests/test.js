8a3d04c0b828dc7f9827070af0d1fc701f28b146
Prashant Rawat <prashantxrawat@gmail.com>
2022-10-31 12:25:07 +0530

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -10,3 +10,9 @@ describe('String', function () {
         });
     });
 });
+
+function add(a,b) {
+    return a + b;
+}
+var expected = add(1,2);
+assert( expected === 3, 'one plus two is three');
